f797f5755aa43d75ee92fa20b2dc6ff64878c332
Pushpa Rajoju <purajoju@in.ibm.com>
2019-07-04 16:46:57 +0530

    Camera plugin chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
